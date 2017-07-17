9e3eb1017cbe28d56be0112e5ad79f1f4f2f428f
杨照珩 <yangzhaoheng@yangzhaohengdeMacBook-Pro.local>
2017-07-17 10:58:52 +0800

    fix fake

--- loan-shop/loan-shop/Fake/ViewController/FakeHomeController.m
+++ loan-shop/loan-shop/Fake/ViewController/FakeHomeController.m
@@ -9,11 +9,15 @@
 #import "FakeHomeController.h"
 #import "ProcessView.h"
 #import "FakeInfoViewController.h"
+#define Rate 4/365
 @interface FakeHomeController ()<UIPickerViewDelegate,UIPickerViewDataSource>
 @property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
 @property (nonatomic , strong) NSArray *pickerArray;
 @property (weak, nonatomic) IBOutlet UIView *topView;
-
+@property (weak, nonatomic) IBOutlet UILabel *rateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *getLabel;
+@property (nonatomic , strong) NSString *day;
+@property (nonatomic , strong) NSString *money;
 @end
 
 @implementation FakeHomeController
@@ -26,6 +30,9 @@
     [_topView addSubview:pv];
     
     _pickerArray = @[@[@"5000",@"2000",@"1000"],@[@"28",@"14",@"7"]];
+    _day = _pickerArray[1][0];
+    _money = _pickerArray[0][0];
+    [self reloadData];
 }
 
 #pragma mark -- delegate
@@ -39,6 +46,20 @@
 
 -(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
     return [_pickerArray[component] objectAtIndex:row];
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
+    if (component==0) {
+        _money = _pickerArray[0][row];
+    }else{
+        _day = _pickerArray[1][row];
+    }
+    [self reloadData];
+}
+
+- (void)reloadData{
+    _rateLabel.text = [NSString stringWithFormat:@"利息：%ld",[_day integerValue]*[_money integerValue]*Rate];
+    _getLabel.text = [NSString stringWithFormat:@"实际到账：%ld",[_money integerValue]-[_day integerValue]*[_money integerValue]*Rate];
 }
 
 - (IBAction)nextStep:(UIButton *)sender {
