cbdae30e44989aa5951d9a532a2aedadf3c532ba
jingyou <yangzhaoheng1949@163.com>
2017-07-24 21:46:00 +0800

    添加图片

--- loan-shop/loan-shop/Fake/View/ProcessView.m
+++ loan-shop/loan-shop/Fake/View/ProcessView.m
@@ -12,6 +12,10 @@
 @property (weak, nonatomic) IBOutlet UILabel *idLabel;
 @property (weak, nonatomic) IBOutlet UILabel *phoneLabel;
 @property (weak, nonatomic) IBOutlet UILabel *loanLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *moneyView;
+@property (weak, nonatomic) IBOutlet UIImageView *idView;
+@property (weak, nonatomic) IBOutlet UIImageView *phoneView;
+@property (weak, nonatomic) IBOutlet UIImageView *parentView;
 
 @end
 @implementation ProcessView
@@ -25,22 +29,35 @@
     _idLabel.textColor = COLOR_GRAY;
     _phoneLabel.textColor = COLOR_GRAY;
     _loanLabel.textColor = COLOR_GRAY;
+    NSString *chooseM = @"chooseM_no";
+    NSString *identity = @"identity_no";
+    NSString *parent = @"parent_no";
+    NSString *phone = @"phone_no";
     switch (index) {
         case 0:
             _moneyLabel.textColor = COLOR_MAIN;
+            chooseM = @"chooseM";
             break;
         case 1:
             _idLabel.textColor = COLOR_MAIN;
+            identity = @"identity";
             break;
         case 2:
             _phoneLabel.textColor = COLOR_MAIN;
+            phone = @"phone";
+            
             break;
         case 3:
             _loanLabel.textColor = COLOR_MAIN;
+            parent = @"parent";
             break;
         default:
             break;
     }
+    [_moneyView setImage:[UIImage imageNamed:chooseM]];
+    [_idView setImage:[UIImage imageNamed:identity]];
+    [_phoneView setImage:[UIImage imageNamed:phone]];
+    [_parentView setImage:[UIImage imageNamed:parent]];
 }
 
 @end
